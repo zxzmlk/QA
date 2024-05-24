ab794309b08fa49ff723ad92cbde93b5d5d3b31d
zxzmlk <99890508+zxzmlk@users.noreply.github.com>
2024-05-24 19:57:01 +0300

    Правки

--- lw9/config/catalogPageConfig.js
+++ lw9/config/catalogPageConfig.js
@@ -1,7 +1,7 @@
 const SEARCH_TEXT = 'TEST';
-const SEARCH_BREADCRUMB = "Поиск по запросу \"TEST\"";
-const SEARCH_PAGE_TITLE = "Поиск по: TEST";
-const SEARCH_URL = 'http://shop.qatl.ru/search?s=TEST';
+const SEARCH_BREADCRUMB = `Поиск по запросу "${SEARCH_TEXT}"`;
+const SEARCH_PAGE_TITLE = `Поиск по: ${SEARCH_TEXT}`;
+const SEARCH_URL = `http://shop.qatl.ru/search?s=${SEARCH_TEXT}`;
 
 const CATEGORY_URL = 'category/men'
 const DEFAULT_PRODUCT_QUANTITY = '1'
